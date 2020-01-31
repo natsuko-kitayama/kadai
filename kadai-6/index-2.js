7cb8a4c714b9a62a089560586e666cc76faf4bac
natsuko-kitayama <na1196kitayama@gmail.com>
2020-01-31 14:54:47 +0000

    課題６を追加

--- kadai-6/index-2.js
+++ kadai-6/index-2.js
@@ -10,6 +10,44 @@ try {
     console.log(JSON.parse(str1))
 } catch (error) {
     console.log(error)
+   // abcという文字列は正しいJSONではないから。
+　 // 正しくは"abc"
+}
 
+try {
+    console.log(JSON.parse(str2))
+} catch (error) {
+    console.log(error)
+   //'abc'という文字列は正しいJSONではないから。
+   //正しくは"abc"
+}
 
+try {
+    console.log(JSON.parse(str3))
+} catch (error) {
+    console.log(error)
+   //"abc"は正しいJSONなのでパースできる。
+}
+
+try {
+    console.log(JSON.parse(str4))
+} catch (error) {
+    console.log(error)
+   //{"a": undefined}という文字列は正しいJSONではないから。
+   //undefinedは値として使えない。
+}
+
+try {
+    console.log(JSON.parse(str5))
+} catch (error) {
+    console.log(error)
+   //{"a": 123}は正しいJSONなのでパースできる。
+}
+
+try {
+    console.log(JSON.parse(str6))
+} catch (error) {
+    console.log(error)
+   //{a: 123}という文字列は正しいJSONではないから。
+   //正しくは{"a": 123}    
 }
